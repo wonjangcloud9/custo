ca18240457f43f98791c5e1805908127ff606a72
notwonjang <venuscwj@naver.com>
2026-04-15 21:45:40 +0900

    feat: register MemberCard, Flashcard, Quiz globally in theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
+import MemberCard from '../components/MemberCard.vue'
+import Flashcard from '../components/Flashcard.vue'
+import Quiz from '../components/Quiz.vue'
 import './style.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('MemberCard', MemberCard)
+    app.component('Flashcard', Flashcard)
+    app.component('Quiz', Quiz)
+  },
+}
